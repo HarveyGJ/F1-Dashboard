2781214bdee210320473b49fafcd6a3df12c787a
Harveys-Laptop <harveygj@hotmail.co.uk>
2026-05-09 21:55:39 +0100

    updating telemetry

--- core/telemetry.py
+++ core/telemetry.py
@@ -5,7 +5,7 @@ fastf1.Cache.enable_cache("~/Programming/F1-Dashboard/cache")
 
 fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme="fastf1")
 
-session = fastf1.get_session(2025, "Silverstone", "Race")
+"""session = fastf1.get_session(2025, "Silverstone", "Race")
 session.load()
 
 
@@ -32,4 +32,54 @@ plt.suptitle(
     f"{session.event["EventName"]} {session.event.year} Race"
 )
 
-plt.show()
+"""
+# dynamically assign variables to names of drivers then discard after ive processed the telemetry
+# change it so i can use a function to get the driver_{idx} lap and tel data
+
+
+def process_tel(session_tel, driver_selection):
+    print(session_tel)
+    print(driver_selection)
+
+    for idx, driver in enumerate(driver_selection):
+        globals()[f"driver_{idx}"] = driver
+
+    driver_0_lap = driver_laps(session_tel, driver_0)
+    driver_1_lap = driver_laps(session_tel, driver_1)
+
+    driver_0_tel = driver_tel(driver_0_lap)
+    driver_1_tel = driver_tel(driver_1_lap)
+    driver_0_color = driver_color(driver_0_lap, session_tel)
+    driver_1_color = driver_color(driver_1_lap, session_tel)
+    fig, ax = plt.subplots()
+    ax.plot(
+        driver_0_tel["Distance"],
+        driver_0_tel["Speed"],
+        color=driver_0_color,
+        label=driver_0,
+    )
+    ax.plot(
+        driver_1_tel["Distance"],
+        driver_1_tel["Speed"],
+        color=driver_1_color,
+        label=driver_1,
+    )
+    ax.legend()
+    plt.suptitle(
+        f"Fastest lap comparison \n"
+        f"{session_tel.event["EventName"]} {session_tel.event.year} Race"
+    )
+
+    return fig
+
+
+def driver_laps(session_tel, driver_name):
+    return session_tel.laps.pick_drivers(driver_name).pick_fastest()
+
+
+def driver_tel(driver_laps):
+    return driver_laps.get_car_data().add_distance()
+
+
+def driver_color(driver_lap, session_tel):
+    return fastf1.plotting.get_team_color(driver_lap["Team"], session=session_tel)
